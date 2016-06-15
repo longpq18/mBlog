eb085791ccce2cad13981a374c782f399b90549c
hklong <longpq18@gmail.com>
2016-06-15 20:49:24 +0700

    update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
-  def public_id
-    return model.name
-  end  
+  # def public_id
+  #   return model.name
+  # end  
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
