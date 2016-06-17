cf5d751b2266d67ffcecea978d7347ca88e47784
hklong <longpq18@gmail.com>
2016-06-17 22:01:04 +0700

    update

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,17 +2,4 @@ module PostsHelper
 	def tag_links(tags)
 	  tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
 	end
-	
-	def tag_cloud(tags, classes)
-	    max = 0
-	    tags.each do |t|
-	      if t.count.to_i > max
-	        max = t.count.to_i
-	      end 
-	    end
-	    tags.each do |tag|
-	      index = tag.count.to_f / max * (classes.size - 1)
-	      yield(tag, classes[index.round])
-	    end
-	end 
 end
