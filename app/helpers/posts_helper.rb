76ab1dc0cc9007526a5914c3eac4f204715e75bd
hklong <longpq18@gmail.com>
2016-06-17 21:37:33 +0700

    update

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,10 +4,15 @@ module PostsHelper
 	end
 	
 	def tag_cloud(tags, classes)
-	  max = tags.sort_by(&:count).last
-	  tags.each do |tag|
-	    index = tag.count.to_f / max.count * (classes.size-1)
-	    yield(tag, classes[index.round])
-	  end
-	end
+	    max = 0
+	    tags.each do |t|
+	      if t.count.to_i > max
+	        max = t.count.to_i
+	      end 
+	    end
+	    tags.each do |tag|
+	      index = tag.count.to_f / max * (classes.size - 1)
+	      yield(tag, classes[index.round])
+	    end
+	end 
 end
